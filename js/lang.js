951b0afcb7e9bf90b714abb9757951bc022f7ba5
Miranquil <miranquil@gmail.com>
2020-07-23 19:20:25 +0800

    update info

--- js/lang.js
+++ js/lang.js
@@ -5,7 +5,7 @@ lang = {
     edit: '',
     data: {
         en: 'Miranquil [200323] Based on 200219',
-        cn: 'Miranquil 绿叶小踢腿@猫小胖[海猫茶屋] [200323] 基于原版 200219'
+        cn: 'Miranquil 拉娜·米果@猫小胖[紫水栈桥] [200323] 基于原版 200219'
     },
     size: {
         1: '55.5%',
